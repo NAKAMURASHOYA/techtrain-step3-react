ba1828e4a79d7397590a4243d2b6282f420dad17
Sho_N <shoya452511@gmail.com>
2025-12-02 00:06:50 +0900

    スレッド一覧画面の基本実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [threads, setThreads] = useState([]);
+  const [error, setError] = useState(null);
+
+useEffect(() => {
+  fetch("https://railway.bulletinboard.techtrain.dev/threads")
+    .then(res => res.json())
+    .then(data => setThreads(data))
+    .catch(err => setError(err))
+}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>スレッド一覧</h1>
+      {error && <div>エラーが発生しました: {error.message}</div>}
+      <ul>
+        {threads.map(thread => (
+          <li key={thread.id}>{thread.title}</li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
